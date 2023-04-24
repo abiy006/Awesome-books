f726f35c2158fb2dc73f6464d51f939be4e2ced1
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-04-25 00:26:03 +0300

    Linter is fixed

--- index.js
+++ index.js
@@ -1,127 +1,83 @@
-const body = document.getElementById('body');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const add = document.querySelector('.add');
+const booksDiv = document.querySelector('.booksDiv');
 
-const myH1 = document.createElement('h1');
-myH1.textContent = 'Awsome Book';
+let arrayOfBooks = [];
 
-const booksContainer = document.createElement('div');
-booksContainer.className = 'booksContainer';
-
-const hrLine = document.createElement('hr');
-
-const myH2 = document.createElement('h2');
-myH2.textContent = 'Add new Book';
-
-const myForm = document.createElement('form');
-myForm.setAttribute('id', 'form');
-myForm.style.display = 'flex';
-myForm.style.flexDirection = 'column';
-myForm.style.gap = '1rem';
-myForm.style.width = '20%';
-
-const input1 = document.createElement('input');
-input1.setAttribute('type', 'text');
-input1.setAttribute('name', 'title');
-input1.setAttribute('placeholder', 'Title');
-input1.className = 'title';
-const input2 = document.createElement('input');
-input2.setAttribute('type', 'text');
-input2.setAttribute('name', 'authore');
-input2.setAttribute('placeholder', 'Authore');
-input2.className = 'authore';
-const button1 = document.createElement('button');
-button1.type = 'submit';
-button1.textContent = 'Add Book';
-
-body.appendChild(myH1);
-body.appendChild(booksContainer);
-body.appendChild(hrLine);
-body.appendChild(myH2);
-
-myForm.appendChild(input1);
-myForm.appendChild(input2);
-myForm.appendChild(button1);
-
-body.appendChild(myForm);
-
-if (localStorage.getItem('Added Books') == null) {
-  localStorage.setItem('Added Books', JSON.stringify([]));
+if (localStorage.getItem('books')) {
+  arrayOfBooks = JSON.parse(localStorage.getItem('books'));
 }
+function addElementsToPageFrom(arrayOfBooks) {
+  booksDiv.innerHTML = '';
 
-let storeData = JSON.parse(localStorage.getItem('Added Books'));
+  arrayOfBooks.forEach((book) => {
+    const div = document.createElement('div');
+    div.className = 'book';
 
-function updateData() {
-  localStorage.setItem('Added Books', JSON.stringify(storeData));
+    div.setAttribute('data-id', book.id);
+    const title = document.createElement('p');
+    title.appendChild(document.createTextNode(book.title));
+    div.appendChild(title);
+    const author = document.createElement('p');
+    author.appendChild(document.createTextNode(book.author));
+    div.appendChild(author);
+    // Create Delete Button
+    const span = document.createElement('button');
+    span.className = 'del';
+    span.appendChild(document.createTextNode('Remove'));
+    // Append Button To Main Div
+    div.appendChild(span);
+    // Add Task Div To Tasks Container
+    booksDiv.appendChild(div);
+  });
 }
-
-function removeBook(x) {
-  const items = JSON.parse(localStorage.getItem('Added Books'));
-  const index = x;
-  items.splice(index, 1);
-  localStorage.setItem('Added Books', JSON.stringify(items));
-
-  storeData = JSON.parse(localStorage.getItem('Added Books'));
-}
-
-function createBooks(arr) {
-  booksContainer.innerHTML = '';
-  for (let i = 0; i < arr.length; i += 1) {
-    const p1 = document.createElement('p');
-    p1.textContent = arr[i].title;
-    p1.style.width = '20%';
-    const p2 = document.createElement('p');
-    p2.textContent = arr[i].authore;
-    p2.style.width = '20%';
-    const button = document.createElement('input');
-    button.setAttribute('class', 'remove-button');
-    button.type = 'button';
-    button.style.width = '20%';
-    button.style.color = 'red';
-    button.value = 'Remove';
-    button.addEventListener('click', () => {
-      removeBook(i);
-      storeData = JSON.parse(localStorage.getItem('Added Books'));
-      createBooks(storeData);
-    });
-    const hr = document.createElement('hr');
-
-    booksContainer.appendChild(p1);
-    booksContainer.appendChild(p2);
-    booksContainer.appendChild(button);
-    booksContainer.appendChild(hr);
+function getDataFromLocalStorage() {
+  const data = window.localStorage.getItem('books');
+  if (data) {
+    const books = JSON.parse(data);
+    addElementsToPageFrom(books);
   }
 }
-function removeBook(x) {
-  const items = JSON.parse(localStorage.getItem('Added Books'));
-  const index = x;
-  items.splice(index, 1);
-  localStorage.setItem('Added Books', JSON.stringify(items));
+getDataFromLocalStorage();
 
-  storeData = JSON.parse(localStorage.getItem('Added Books'));
-
-  createBooks(storeData);
+function addDataToLocalStorageFrom(arrayOfBooks) {
+  window.localStorage.setItem('books', JSON.stringify(arrayOfBooks));
 }
 
-function addNewdata(bookTitle, bookAuthore) {
-  const Book = {
-    title: bookTitle,
-    authore: bookAuthore,
+function deleteTaskWith(bookId) {
+  arrayOfBooks = arrayOfBooks.filter((book) => book.id !== +bookId);
+  addDataToLocalStorageFrom(arrayOfBooks);
+}
+function addBookToArray(title, author) {
+  // Task Data
+  const book = {
+    id: Date.now(),
+    title,
+    author,
   };
-  storeData.push(Book);
-  updateData();
-  createBooks(storeData);
+    // Push Task To Array Of Tasks
+  arrayOfBooks.push(book);
+  // Add Tasks To Page
+  addElementsToPageFrom(arrayOfBooks);
+  // Add Tasks To Local Storage
+  addDataToLocalStorageFrom(arrayOfBooks);
 }
+add.onclick = function () {
+  if (title.value !== '' && author.value !== '') {
+    addBookToArray(title.value, author.value);
+    title.value = '';
+    author.value = '';
+  }
+};
 
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  const title = document.querySelector('.title');
-  const authore = document.querySelector('.authore');
-  e.preventDefault();
-  addNewdata(title.value, authore.value);
+booksDiv.addEventListener('click', (e) => {
+  // Delete Button
+  if (e.target.classList.contains('del')) {
+    // Remove Task From Local Storage
+    deleteTaskWith(e.target.parentElement.getAttribute('data-id'));
+    // Remove Element From Page
+    e.target.parentElement.remove();
+  }
+  // Task Element
 });
-
-// function displayBooks() {
-//   createBooks(storeData);
-//   }
-
-window.onload = createBooks(storeData);
-/* eslint no-use-before-define: off */
