4f96f3f9bdb3cdc163973f07e7f4683312237237
ahmed025566 <ahmedsalah025566@gmail.com>
2023-04-24 19:17:09 +0200

    Fix linter errors

--- index.js
+++ index.js
@@ -5,84 +5,81 @@ const booksDiv = document.querySelector('.booksDiv');
 
 let arrayOfBooks = [];
 
-if (localStorage.getItem("books")) {
-    arrayOfBooks = JSON.parse(localStorage.getItem("books"));
-  }
+if (localStorage.getItem('books')) {
+  arrayOfBooks = JSON.parse(localStorage.getItem('books'));
+}
+function addElementsToPageFrom(arrayOfBooks) {
+  // Empty Tasks Div
+  booksDiv.innerHTML = '';
+  // Looping On Array Of Tasks
+  arrayOfBooks.forEach((book) => {
+    // Create Main Div
+    const div = document.createElement('div');
+    div.className = 'book';
 
+    div.setAttribute('data-id', book.id);
+    const title = document.createElement('p');
+    title.appendChild(document.createTextNode(book.title));
+    div.appendChild(title);
+    const author = document.createElement('p');
+    author.appendChild(document.createTextNode(book.author));
+    div.appendChild(author);
+    // Create Delete Button
+    const span = document.createElement('button');
+    span.className = 'del';
+    span.appendChild(document.createTextNode('Remove'));
+    // Append Button To Main Div
+    div.appendChild(span);
+    // Add Task Div To Tasks Container
+    booksDiv.appendChild(div);
+  });
+}
+function getDataFromLocalStorage() {
+  const data = window.localStorage.getItem('books');
+  if (data) {
+    const books = JSON.parse(data);
+    addElementsToPageFrom(books);
+  }
+}
 getDataFromLocalStorage();
 
-add.onclick = function () {
-    if(title.value !== '' && author.value !== '') {
-        addBookToArray(title.value, author.value);
-        title.value = '';
-        author.value = '';
-    }
+function addDataToLocalStorageFrom(arrayOfBooks) {
+  window.localStorage.setItem('books', JSON.stringify(arrayOfBooks));
 }
 
-booksDiv.addEventListener("click", (e) => {
-    // Delete Button
-    if (e.target.classList.contains("del")) {
-      // Remove Task From Local Storage
-      deleteTaskWith(e.target.parentElement.getAttribute("data-id"));
-      // Remove Element From Page
-      e.target.parentElement.remove();
-    }
-    // Task Element
-    
-  });
-  function addBookToArray(title, author) {
-    // Task Data
-    const book = {
-      id: Date.now(),
-      title: title,
-      author : author
-    };
+function deleteTaskWith(bookId) {
+  arrayOfBooks = arrayOfBooks.filter((book) => book.id !== +bookId);
+  addDataToLocalStorageFrom(arrayOfBooks);
+}
+function addBookToArray(title, author) {
+  // Task Data
+  const book = {
+    id: Date.now(),
+    title,
+    author,
+  };
     // Push Task To Array Of Tasks
-    arrayOfBooks.push(book);
-    // Add Tasks To Page
-    addElementsToPageFrom(arrayOfBooks);
-    // Add Tasks To Local Storage
-    addDataToLocalStorageFrom(arrayOfBooks);
+  arrayOfBooks.push(book);
+  // Add Tasks To Page
+  addElementsToPageFrom(arrayOfBooks);
+  // Add Tasks To Local Storage
+  addDataToLocalStorageFrom(arrayOfBooks);
+}
+add.onclick = function () {
+  if (title.value !== '' && author.value !== '') {
+    addBookToArray(title.value, author.value);
+    title.value = '';
+    author.value = '';
   }
-  function addElementsToPageFrom(arrayOfBooks) {
-    // Empty Tasks Div
-    booksDiv.innerHTML = "";
-    // Looping On Array Of Tasks
-    arrayOfBooks.forEach((book) => {
-      // Create Main Div
-      let div = document.createElement("div");
-      div.className = "book";
-           
-      div.setAttribute("data-id", book.id);
-     const title = document.createElement('p');
-     title.appendChild(document.createTextNode(book.title));
-     div.appendChild(title);
-     const author = document.createElement('p');
-     author.appendChild(document.createTextNode(book.author));
-     div.appendChild(author);
-      // Create Delete Button
-      let span = document.createElement("button");
-      span.className = "del";
-      span.appendChild(document.createTextNode("Remove"));
-      // Append Button To Main Div
-      div.appendChild(span);
-      // Add Task Div To Tasks Container
-      booksDiv.appendChild(div);
-    });
-  }
-  function addDataToLocalStorageFrom(arrayOfBooks) {
-    window.localStorage.setItem("books", JSON.stringify(arrayOfBooks));
-  }
+};
 
-  function getDataFromLocalStorage() {
-    let data = window.localStorage.getItem("books");
-    if (data) {
-      let books = JSON.parse(data);
-      addElementsToPageFrom(books);
-    }
+booksDiv.addEventListener('click', (e) => {
+  // Delete Button
+  if (e.target.classList.contains('del')) {
+    // Remove Task From Local Storage
+    deleteTaskWith(e.target.parentElement.getAttribute('data-id'));
+    // Remove Element From Page
+    e.target.parentElement.remove();
   }
-  function deleteTaskWith(bookId) {
-    
-    arrayOfBooks = arrayOfBooks.filter((book) => book.id != bookId);
-    addDataToLocalStorageFrom(arrayOfBooks);
-  }
+  // Task Element
+});
